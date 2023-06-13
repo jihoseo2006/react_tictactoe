09e3aeb58f5aafcbfba5c352b5a57110c56f1a79
jihoseo2006 <jihoseo2006@gmail.com>
2023-06-13 20:10:02 +0900

    Square.css height 속성 누락추가
    이러니 박스가 ㅂㅅ이지.....

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,12 +2,21 @@ import React from "react"
 import "./Square.css";
 
 export default class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+
+  }
+
+
   render() {
     return (
     <button className="square" 
-      onClick={() => {console.log('click') }}
-    >
-       {this.props.value}
+      onClick={() => {this.setState({ value: 'X'}) }}
+      >
+       {this.state.value}
     </button>
     )
   }
