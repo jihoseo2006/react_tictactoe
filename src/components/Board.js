b362824bab16c50c46c62a1c9864f73fb75b01c3
jihoseo2006 <jihoseo2006@gmail.com>
2023-07-03 19:33:50 +0900

    winner select, change square style
    승자 결정 후 화면에 승자: 플레이어 형식으로 구현되는 로직 구현, square 컴포넌트 스타일 수정

--- src/components/Board.js
+++ src/components/Board.js
@@ -10,29 +10,51 @@ const Board  = () => {
   
   const calculateWinner = (squares) => {
     const lines = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
     ]
     for( let index = 0; index < lines.length; index++){
       const [a,b,c] = lines[index];
-      if(squares[a] && squares[a] === squares[b])
+      if(squares[a] 
+        && squares[a] === squares[b] 
+        && squares[a] === squares[c])
+        {
+          console.log('squares[a]', squares[a]);
+        return squares[a];
+      }
     }
+    return null;
   }
 
   const winner = calculateWinner(squares);
-  const status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  
+  let status;
+  if(winner){
+    status = 'Winner:' + winner;
+  }else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
 
   const handleClick = (i) => {
     const newSquares = squares.slice();
+    console.log('newSquares', newSquares);
+    console.log('newSquares[i]', newSquares[i]);
+    if(calculateWinner(newSquares) || newSquares[i])
+    {
+      return;
+    }
+
+
     newSquares[i] = xIsNext ? 'X': 'O';
     setSquares(newSquares);
     setxIsNext(prev => !prev);
+    
   }
 
 
