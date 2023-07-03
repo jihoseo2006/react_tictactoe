e6b18cbe9308e9d751fb48d73509a4b75ea74a14
jihoseo2006 <jihoseo2006@gmail.com>
2023-07-03 20:28:31 +0900

    app.js 로 상태 옮기기
    로그를 남기기 위해서 코드를 뭉탱이로 옮겨보아요!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { useState } from "react";
 import "./App.css"
 import Board from "./components/Board";
 
 function App() {
+  
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null)}]);
+  const [xIsNext, setxIsNext] = useState(true);
+
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+    for( let index = 0; index < lines.length; index++){
+      const [a,b,c] = lines[index];
+      if(squares[a] 
+        && squares[a] === squares[b] 
+        && squares[a] === squares[c])
+        {
+          console.log('squares[a]', squares[a]);
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const winner = calculateWinner(squares);
+  
   return (
     <div className="game">
       <div className="game-board">
@@ -12,6 +44,6 @@ function App() {
       </div>
     </div>
   );
-}
+} 
 
 export default App;
--- src/components/Board.js
+++ src/components/Board.js
@@ -7,33 +7,6 @@ const Board  = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setxIsNext] = useState(true);
   
-  
-  const calculateWinner = (squares) => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ]
-    for( let index = 0; index < lines.length; index++){
-      const [a,b,c] = lines[index];
-      if(squares[a] 
-        && squares[a] === squares[b] 
-        && squares[a] === squares[c])
-        {
-          console.log('squares[a]', squares[a]);
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
-  const winner = calculateWinner(squares);
-  
   let status;
   if(winner){
     status = 'Winner:' + winner;
@@ -45,6 +18,7 @@ const Board  = () => {
     const newSquares = squares.slice();
     console.log('newSquares', newSquares);
     console.log('newSquares[i]', newSquares[i]);
+    
     if(calculateWinner(newSquares) || newSquares[i])
     {
       return;
